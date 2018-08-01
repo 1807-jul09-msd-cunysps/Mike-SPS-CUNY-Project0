f01212174a20d4d1c99b51daa29be4422dcfd171
Michael Corso <mcorso21@gmail.com>
2018-08-01 12:27:00 -0400

    Delete SqlShowAll.sql

--- SqlShowAll.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT CONCAT(p.firstname, ' ', p.lastname) AS Name,
-	   CONCAT(addr.housenum, ' ', addr.street, ' ', addr.zipcode) AS Address,
-	   CONCAT(ph.number, ' ', ph.ext) AS Phone
-FROM person AS p
-INNER JOIN phone AS ph ON (p.Id = ph.personID)
-INNER JOIN address AS addr ON (p.Id = addr.personID)
